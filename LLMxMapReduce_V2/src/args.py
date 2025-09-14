03b533c5ef96446f00b62720117d6f1a0192b1fb
lizhenyu0167-art <lizhenyu0167@gmail.com>
2025-09-14 23:29:18 +0800

    上传LLMxMapReduce_V2/src/args.py文件

---
+++ LLMxMapReduce_V2/src/args.py
@@ -0,0 +1,33 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Run the EntirePipeline with specified input, output, and prompt files.")
+    
+    parser.add_argument("--topic", type=str, help="Your research topic")
+    parser.add_argument("--description", type=str, help="Description about your research topic. It will be used to retrieve pages.")
+    parser.add_argument("--top_n", type=int, default=10, help="Number of top n references to retrieve")
+    parser.add_argument("--max_results", type=int, default=30, help="Maximum number of results to return from arXiv API per query")
+    
+    parser.add_argument("--input_file", type=str, help="Path to the input file")
+    parser.add_argument("--output_file", type=str, required=True, help="Path to the output file")
+    parser.add_argument("--config_file", type=str, default='config\model_config.json', help="Path to the config json file")
+    
+    parser.add_argument("--data_num", type=int, default=None, help="Number of data to process") 
+    parser.add_argument("--parallel_num", type=int, default=4, help="Number of data to process concurrently in pipeline") # 原默认值是1
+    
+    parser.add_argument("--digest_group_mode", type=str, choices=["random", "llm"], default="llm", help="Group paper to digest mode")
+    parser.add_argument("--skeleton_group_size", type=int, default=1, help="Number of digest to generate a skeleton")   # 原默认值是3
+    
+    parser.add_argument("--block_count", type=int, default=2, help="Number of max iteration blocks")    # 原默认值是0
+    parser.add_argument("--output_each_block", type=bool, default=False, help="Whether to output each block")
+    
+    parser.add_argument("--conv_layer", type=int, default=3, help="Number of convolution layer, only mcts need.")  # 原默认值是6
+    parser.add_argument("--conv_kernel_width", type=int, default=3, help="Receptive field of convolution layer, how many old suggestion to generate new suggestion")
+    parser.add_argument("--conv_result_num", type=int, default=5, help="Number of suggestion to generate in each convolution layer")    # 原默认值是10
+    parser.add_argument("--top_k", type=int, default=6, help="Number of top k suggestion that will be kept in mcts")
+    
+    parser.add_argument("--self_refine_count", type=int, default=1, help="Number of self-refine cycle")  # 原默认值是3
+    parser.add_argument("--self_refine_best_of", type=int, default=3, help="Number of result number in each cycle")
+    args = parser.parse_args()
+    return args
